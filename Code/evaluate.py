f037ee34798caa26485aceb80e5f0e96237631a7
Joan Falco-Roget <kaljandlux@gmail.com>
2024-06-27 18:32:16 +0200

    implemented inference procedure

--- Code/evaluate.py
+++ Code/evaluate.py
@@ -2,11 +2,67 @@
 import torch
 import torch.nn.functional as F
 from tqdm import tqdm
-import os
+import os, glob
 import nibabel as nib
 import numpy as np
 import matplotlib.pylab as plt
+import logging
 
+@torch.inference_mode()
+def compute_segmentation(data_path, model, results_path, device, threshold):
+    logging.basicConfig(level=logging.INFO)
+    logging.info(" Creating results directory")
+    # Generate the directory to store the results
+    if not os.path.exists(results_path):
+        os.makedirs(results_path)
+
+    # Find images
+    logging.info(" Loading images")
+    files = glob.glob(os.path.join(data_path,"*.nii.gz"))
+
+    # Run inference
+    logging.info(" Segmenting images")
+    for i, f in enumerate(tqdm(files)):
+        name = f.split("/")[-1].split(".")[0]
+        # Load data
+        img = nib.load(f)
+        data, affine = img.get_fdata(), img.affine, 
+        try:
+            header = img.header
+        except:
+            header = None
+        MRI = torch.tensor(
+                np.expand_dims(
+                    np.expand_dims(
+                        data, axis=0 # [1,1,320,320,10]
+                    ), axis=0
+                )
+            ).to(device=device, dtype=torch.float32)
+        output = model(MRI)
+        probs = F.sigmoid(output)
+        mask = (probs > threshold).int()
+        # Generate preliminary image
+        fig, ax = plt.subplots(3,3,figsize=(15,15))
+        ax[0,0].set_title("Image", fontsize=15, fontweight='bold')
+        ax[0,1].set_title("Probability map", fontsize=15, fontweight='bold')
+        ax[0,2].set_title("Mask", fontsize=15, fontweight='bold')
+        for j in range(3):
+            ax[j,0].imshow(MRI[0].squeeze().cpu()[:,:,3+j*2])
+            ax[j,1].imshow(probs[0].squeeze().cpu()[:,:,3+j*2])
+            ax[j,2].imshow(mask[0].squeeze().cpu()[:,:,3+j*2])
+            ax[j,0].spines[["top","right","bottom","left"]].set_visible(False)
+            ax[j,1].spines[["top","right","bottom","left"]].set_visible(False)
+            ax[j,2].spines[["top","right","bottom","left"]].set_visible(False)
+            ax[j,0].set_xticks([]), ax[j,0].set_yticks([])
+            ax[j,1].set_xticks([]), ax[j,1].set_yticks([])
+            ax[j,2].set_xticks([]), ax[j,2].set_yticks([])
+        fig.tight_layout()
+        plt.savefig(f"{results_path}/{name}.png")
+        plt.close()
+        # Save as nifti
+        nib.save(nib.Nifti1Image(mask[0,0].cpu().numpy(), affine), f"{results_path}/{name}_segmentation.nii.gz")
+        nib.save(nib.Nifti1Image(probs[0,0].cpu().numpy(), affine), f"{results_path}/{name}_probability.nii.gz")
+    logging.info(" Done!")
 
 @torch.inference_mode()
 def evaluate(net, dataloader, device, amp, dice, epoch, fold=None, th=0.5):
--- Code/tumorigenesis.py
+++ Code/tumorigenesis.py
@@ -1,8 +1,9 @@
 import argparse
 import yaml
-import os
+import os, glob
 from unet.unet_model import UNet
 from train import train_model, train_model_kfold
+from evaluate import compute_segmentation
 import torch
 
 # Define argparse
@@ -12,8 +13,10 @@ sub_parser = parser.add_subparsers()
 compute = sub_parser.add_parser('compute', help="Use this argument to segment tumorigenic areas with a trained AI model")
 compute.add_argument('test_folder', type=str, help="Provide the directory storing the MRI(s)")
 compute.add_argument('model_path', type=str, help="Provide the direcory storing the trained model")
+compute.add_argument('model_name', type=str, default=None, help="Provide aproxy for the name of the model file")
 compute.add_argument('to_save', type=str, help="Indicate the directory to save the predicted tumorigenic regions")
-compute.add_argument('--model_name', type=str, default=None, help="Provide the name of the model file")
+compute.add_argument('--threshold', type=float, default=.5, help="Indicate the threshold to binarize the probability maps")
+compute.add_argument('--device', type=str, choices=['cpu', 'cuda'], default='cpu', help="Run inference on CPU or GPU")
 compute.add_argument('--mode', default='compute')
 # Train the model
 train = sub_parser.add_parser('train', help="Use this argument to train an AI model to detect tumorigenic areas in MRI(s)")
@@ -24,11 +27,11 @@ args = parser.parse_args()
 mode = args.mode
 
 if __name__ == '__main__':
+    # AI model
+    model = UNet(n_channels=1, n_classes=1)
+    
+    # Training configuration
     if mode=="train":
-        # AI model
-        model = UNet(n_channels=1, n_classes=1)
-
-        # Training configuration
         config_file = os.path.join(args.configuration,"config.yaml")
         with open(config_file, 'r') as f:
             config = yaml.load(f, Loader=yaml.FullLoader)
@@ -69,7 +72,20 @@ if __name__ == '__main__':
                 n_train = float(config["n_train"]),
                 val_freq = int(config["validation_frequency"])
         )
+    # Inference configuration
     elif mode=="compute":
-        print('compute')
+        # Load the model
+        model_path = glob.glob(os.path.join(args.model_path,args.model_name)+"*")[0]
+        model.load_state_dict(
+            torch.load(model_path, map_location=args.device)
+        )
+        # Compute the segmentation
+        compute_segmentation(
+            data_path = args.test_folder,
+            results_path = args.to_save,
+            model = model,
+            device = args.device,
+            threshold = args.threshold
+        )
     else:
         raise ValueError("No mode provided through command line interface; try python tumorigenic.py --help")
